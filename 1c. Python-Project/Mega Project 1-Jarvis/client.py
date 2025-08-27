f7ea5dcaff2cab0bea68849036afd919e1e5b198
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2025-08-28 02:44:03 +0530

    done

--- 1c. Python-Project/Mega Project 1-Jarvis/client.py
+++ 1c. Python-Project/Mega Project 1-Jarvis/client.py
@@ -1,8 +1,7 @@
+from config import api_key
 from openai import OpenAI
 
-client = OpenAI(
-    api_key="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"
-)
+client = OpenAI(api_key=api_key)
 
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
