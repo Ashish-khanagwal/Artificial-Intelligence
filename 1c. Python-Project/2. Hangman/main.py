4af1e088d804fb3abc74f79e8d4749d346eece61
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2025-08-28 14:55:59 +0530

    Hangman done

---
+++ 1c. Python-Project/2. Hangman/main.py
@@ -0,0 +1,59 @@
+import random
+import string
+
+from words import words
+
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while "-" in word or " " in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # letters user has guessed
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
+
+        # letters used
+        print(
+            "You have",
+            lives,
+            "lives left, you have used these letters: ",
+            " ".join(used_letters),
+        )
+
+        # what current word is (W - R D)
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+        print("Current word: ", " ".join(word_list))
+
+        # User input
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+            else:
+                lives = lives - 1
+                print("Letter is not in word!")
+
+        elif user_letter in used_letters:
+            print("You have already guessed the character, please try again! ")
+
+        else:
+            print("Invalid character. Please try again!")
+
+    if lives == 0:
+        print("You died, the word was: ", word)
+    else:
+        print("You guessed the word: ", word, "!!")
+
+
+hangman()
