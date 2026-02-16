4bc2403001907cdb6e9eec7e5322c77a743b459c
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2026-02-16 22:21:10 +0530

    DATA CLEANING

---
+++ demo/main.py
@@ -0,0 +1,14 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "requests>=2.32.5",
+#     "rich>=14.3.2",
+# ]
+# ///
+
+import sys
+
+import requests
+from rich import print
+
+print(sys.version)
